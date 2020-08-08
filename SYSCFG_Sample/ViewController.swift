d73b1947574a79556eaa1dc3b58878defd8eb64f
Jan Fabel <j4nf4b3l@outlook.com>
2020-08-08 21:17:25 +0200

    Initial Commit

---
+++ SYSCFG_Sample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SYSCFG_Sample
+//
+//  Created by Jan Fabel on 08.08.20.
+//  Copyright © 2020 Jan Fabel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
